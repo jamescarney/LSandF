02d9d9dbc77c98a0604da8a4683b4002507247a1
James Carney <james.carneydev@gmail.com>
2014-12-21 16:14:12 -0500

    Changed to PHP
    Changed everything to .php

---
+++ app/goals.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+
+		    <section class="main-section">
+			    
+				<div class="wrapper">
+
+					<div class="begin">
+						<h1>Welcome!</h1>
+						<p class="lead">Start here to put in some quick information so we can help assess where you are health wise so we can point you to the right direction for where to begin your fitness journey with us.</p>
+						
+					</div>
+
+				</div>
+				<!-- /wrapper -->
+
+
+		      	<?php include('footer.php'); ?>
---
+++ app/index.php
@@ -0,0 +1,25 @@
+<?php include('header.php'); ?>
+
+					<section class="main-section">
+						<div class="row">
+							<div class="small-8 small-centered columns">
+								<h1>Welcome to Lifestyle Strength and Fitness</h1>
+								<p class="lead">The App that Does Everything Fitness</p>
+							</div>
+							<form>
+								<div class="small-8 small-centered columns">
+									<input type="text" placeholder="Email" />
+								</div>
+								<div class="small-8 small-centered columns">
+									<input type="password" placeholder="Password" />
+								</div>
+								<div class="small-8 small-centered columns sign-in">
+									<a href="home.php" class="button expand success sign-in">Sign In</a>
+								</div>
+								<div class="columns question text-center">
+									<p><a href="#">Forgot your password?</a></p>
+									<p><a href="#">Sign up.</a></p>
+								</div>
+							</form> 
+						</div>
+						<?php include('footer.php'); ?>
